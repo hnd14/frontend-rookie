3efba58aa25383f2faf215967904eca418b1bc71
hnd14 <hnd14@case.edu>
2024-05-28 22:28:51 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { LoginPage } from "./admin/login/LoginPage.jsx";
+import { LoginPage } from "./admin/login/LoginPage.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./store/home/Home.tsx";
+import Admin from "./admin/Admin.tsx";
+import CategoryPage from "./admin/category/CategoryPage.tsx";
+import ProductPage from "./admin/product/ProductPage.tsx";
+import NewCategoryPage from "./admin/new-category/NewCategoryPage.tsx";
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" exact Component={Home} />
+        <Route path="/login" Component={LoginPage} />
+        <Route path="/admin" Component={Admin}>
+          <Route index path="/admin" Component={ProductPage} />
+          <Route path="/admin/categories" Component={CategoryPage} />
+          <Route path="/admin/new-categories" Component={NewCategoryPage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
