f94f88440ed6420f8e61692a091a5c036b27033b
hnd14 <hnd14@case.edu>
2024-05-29 10:41:24 +0700

    Add products services

--- src/admin/services/AdminService.ts
+++ src/admin/services/AdminService.ts
@@ -1,5 +1,6 @@
-import { LoginData } from "../login/model/LoginData";
+import { LoginData } from "../../login/model/LoginData";
 import axios from "axios";
+import { NewProductItem } from "../product/model/NewProductItem";
 
 const STORE_BACK_API = "http://localhost:8080/store-back";
 
@@ -19,9 +20,10 @@ export async function login(data: LoginData) {
   return response;
 }
 
-export const getAllCategories = (pageNumber) => {
+export const getAllCategories = (pageNumber: number) => {
   return axios
-    .get(STORE_BACK_API + "/categories?pageNumber=" + pageNumber.toString(), {
+    .get(STORE_BACK_API + "/categories?", {
+      params: { pageNumber: pageNumber.toString() },
       withCredentials: true,
     })
     .then((res) => res.data);
@@ -44,6 +46,33 @@ export async function updateCategory(id: number, desc: string) {
 
 export async function deleteCategory(id: number) {
   return axios.delete(STORE_BACK_API + "/categories/" + id.toString(), {
+    withCredentials: true,
+  });
+}
+
+export const getAllProducts = (pageNumber: number) => {
+  return axios
+    .get(STORE_BACK_API + "/products", {
+      params: { pageNumber: pageNumber.toString() },
+      withCredentials: true,
+    })
+    .then((res) => res.data);
+};
+
+export async function createNewProducts(data: NewProductItem) {
+  return axios.post(STORE_BACK_API + "/products", data, {
+    withCredentials: true,
+  });
+}
+
+export async function updateProduct(id: number, data: NewProductItem) {
+  return axios.put(STORE_BACK_API + "/categories/" + id.toString(), data, {
+    withCredentials: true,
+  });
+}
+
+export async function deleteProduct(id: number) {
+  return axios.delete(STORE_BACK_API + "/products/" + id.toString(), {
     withCredentials: true,
   });
 }
