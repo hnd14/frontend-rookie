cedf0e6b11cbd6d554068c2972e05e5e97e99105
hnd14 <hnd14@case.edu>
2024-05-29 14:05:53 +0700

    Handle clicking edit button

--- src/admin/product/components/ProductRow.tsx
+++ src/admin/product/components/ProductRow.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { ProductItem } from "../model/ProductItem";
+import { Container, FormCheck } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 interface Props {
   product: ProductItem;
   handleDelete;
 }
 const ProductRow = ({ product, handleDelete }: Props) => {
+  const nav = useNavigate();
+  const handleEdit = () => {
+    nav(`/admin/products/${product.id}`);
+  };
   return (
     <tr>
       <td>{product.name}</td>
       <td>{product.desc}</td>
       <td>{product.salePrice}</td>
-      <td>{product.isFeatured}</td>
       <td>{product.stock}</td>
       <td>
-        <button>Edit</button>
+        <Container fluid>
+          <FormCheck disabled checked={product.isFeatured} />
+        </Container>
+      </td>
+      <td>
+        <button onClick={handleEdit}>Edit</button>
         <button
           onClick={() => {
             handleDelete(product.id);
