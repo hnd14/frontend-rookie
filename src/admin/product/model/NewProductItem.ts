91569b7af30b03b797ba19e662d30f28d9b2528a
hnd14 <hnd14@case.edu>
2024-05-29 10:41:41 +0700

    Add product dtos

---
+++ src/admin/product/model/NewProductItem.ts
@@ -0,0 +1,8 @@
+export type NewProductItem = {
+  id: number;
+  name: string;
+  desc: string;
+  salePrice: number;
+  stock: number;
+  isFeatured: boolean;
+};
---
+++ src/admin/product/model/ProductItem.ts
@@ -0,0 +1,12 @@
+export type ProductItem = {
+  id: number;
+  name: string;
+  desc: string;
+  salePrice: number;
+  stock: number;
+  isFeatured: boolean;
+  createdBy: string;
+  createdTime: string;
+  updatedBy: string;
+  updatedTime: string;
+};
