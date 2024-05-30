7d062721e80a6981660dc6e69d274004017d4047
hnd14 <hnd14@case.edu>
2024-05-30 17:44:36 +0700

    Reformat product carousel

--- src/store/components/ProductCarousel.tsx
+++ src/store/components/ProductCarousel.tsx
@@ -6,8 +6,9 @@ import ProductCard from "./ProductCard.tsx";
 import { Col, Container, Row } from "react-bootstrap";
 interface Props {
   data: ProductThumbnail[];
+  title: String;
 }
-const ProductCarousel = ({ data }: Props) => {
+const ProductCarousel = ({ data, title = "" }: Props) => {
   const [index, setIndex] = useState(0);
   const [cardsPerSlide, setCardsPerSlide] = useState(4);
   const pixelToRem = parseFloat(
@@ -16,18 +17,18 @@ const ProductCarousel = ({ data }: Props) => {
 
   useEffect(() => {
     setCardsPerSlide(
-      Math.max(1, Math.floor(window.innerWidth / pixelToRem / 18) - 1)
+      Math.max(1, Math.round(window.innerWidth / pixelToRem / 18) - 1)
     );
 
     const updateMedia = () => {
       setCardsPerSlide(
-        Math.max(1, Math.floor(window.innerWidth / pixelToRem / 18) - 1)
+        Math.max(1, Math.round(window.innerWidth / pixelToRem / 18) - 1)
       );
+      setIndex(0);
     };
     window.addEventListener("resize", updateMedia);
     return () => window.removeEventListener("resize", updateMedia);
   }, [pixelToRem]);
-
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
@@ -53,25 +54,33 @@ const ProductCarousel = ({ data }: Props) => {
   items.push(
     <Carousel.Item as={Col} className="col-10">
       <Row>
-        {cards.slice(cards.length - cardsPerSlide, cards.length).map((card) => (
-          <Col>{card}</Col>
-        ))}
+        {cards
+          .slice(Math.max(0, cards.length - cardsPerSlide), cards.length)
+          .map((card) => (
+            <Col>{card}</Col>
+          ))}
       </Row>
     </Carousel.Item>
   );
 
   return (
-    <Container className="d-flex justify-content-center">
-      <Carousel
-        activeIndex={index}
-        onSelect={handleSelect}
-        variant="dark"
-        indicators={false}
-        as={Row}
-      >
-        {items}
-      </Carousel>
-    </Container>
+    <>
+      <Container>
+        <h2>{title}</h2>
+
+        <Container className="d-flex justify-content-center">
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            variant="dark"
+            indicators={false}
+            as={Row}
+          >
+            {items}
+          </Carousel>
+        </Container>
+      </Container>
+    </>
   );
 };
 
