17f7095b875c59b8c69493f912031e15755860b9
hnd14 <hnd14@case.edu>
2024-05-30 17:43:13 +0700

    Create a product list page and a search bar

---
+++ src/store/components/SearchBar.tsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from "react";
+import {
+  Container,
+  Form,
+  InputGroup,
+  DropdownButton,
+  Dropdown,
+  Button,
+} from "react-bootstrap";
+import useSWR from "swr";
+import { storeFetcher } from "../services/CustomerServices.ts";
+import { useNavigate } from "react-router-dom";
+let category_id = null;
+const SearchBar = () => {
+  const [chosenCategory, setChosenCategory] = useState("Categories");
+  const nav = useNavigate();
+  const nameRef = useRef(document.createElement("input"));
+  const params = {
+    pageSize: 100,
+  };
+  const { data, error, isLoading } = useSWR(
+    ["/categories", params],
+    ([url, params]) => storeFetcher(url, params)
+  );
+  console.log(category_id);
+  if (error) return <h1>Error</h1>;
+  if (isLoading) return <h1>Loading</h1>;
+  const handleSearch = () => {
+    if (category_id) {
+      nav(`/products?name=${nameRef.current.value}&category=${category_id}`);
+    } else {
+      nav(`/products?name=${nameRef.current.value}`);
+    }
+  };
+  return (
+    <Container>
+      <h2>Search</h2>
+      <Form>
+        <InputGroup>
+          <DropdownButton
+            variant="outline-secondary"
+            title={chosenCategory}
+            align="end"
+          >
+            {data.content.map((category) => (
+              <Dropdown.Item
+                onClick={() => {
+                  category_id = category.id;
+                  setChosenCategory(category.name);
+                }}
+              >
+                {category.name}
+              </Dropdown.Item>
+            ))}
+            <Dropdown.Item
+              onClick={() => setChosenCategory("")}
+            ></Dropdown.Item>
+          </DropdownButton>
+          <Form.Control type="text" ref={nameRef} />
+          <Button variant="outline-secondary" onClick={handleSearch}>
+            Search
+          </Button>
+        </InputGroup>
+      </Form>
+    </Container>
+  );
+};
+
+export default SearchBar;
