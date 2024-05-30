014d4b7a960f1e83f6fb9db3f498b0fd3aa0ef4f
hnd14 <hnd14@case.edu>
2024-05-30 17:42:50 +0700

    Update home page

--- src/store/home/Home.tsx
+++ src/store/home/Home.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import ProductCarousel from "../components/ProductCarousel.tsx";
 import { storeFetcher } from "../services/CustomerServices.ts";
 import useSWR from "swr";
+import SearchBar from "../components/SearchBar.tsx";
 
 const Home = () => {
-  const { data, error, isLoading } = useSWR("/products", storeFetcher);
+  const featuredParams = {
+    pageSize: 20,
+    pageNumber: 1,
+    isFeatured: true,
+  };
+  const {
+    data: featuredData,
+    error,
+    isLoading,
+  } = useSWR(["/products", featuredParams], ([url, params]) =>
+    storeFetcher(url, params)
+  );
+
   if (error) return <h1>Error</h1>;
   if (isLoading) return <h1>Loading</h1>;
-
   return (
     <>
-      <h1>Welcome!</h1>
-      <ProductCarousel data={data} />
+      <SearchBar />
+      <ProductCarousel title="Featured product" data={featuredData.content} />
     </>
   );
 };
