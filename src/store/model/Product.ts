22adf2da166e238ab8459718cf4cf0f9ba895aff
hnd14 <hnd14@case.edu>
2024-05-30 14:02:21 +0700

    products dto for customer

---
+++ src/store/model/Product.ts
@@ -0,0 +1,15 @@
+export type ProductThumbnail = {
+  id: number;
+  name: string;
+  salePrice: number;
+  thumbnail_src: string;
+};
+export type ProductDetails = {
+  id: number;
+  name: string;
+  salePrice: number;
+  thumbnail_src: string;
+  desc: string;
+  rating: number;
+  img_src: string[];
+};
