d7cb0b17eb8337fcc41c2ce6d5e7fc7768182a17
hnd14 <hnd14@case.edu>
2024-06-05 15:43:31 +0700

    Add rating time

--- src/store/product-details/components/RatingCard.tsx
+++ src/store/product-details/components/RatingCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import StaticStar from "./StaticStar.tsx";
+import { formatTime } from "../../../util/Util.ts";
 
 const RatingCard = ({ data }) => {
   return (
@@ -15,6 +16,11 @@ const RatingCard = ({ data }) => {
       <Card.Body>
         <Card.Text>{data.comment}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <Card.Text>
+          <i>{`${formatTime(data.updatedTime)}`}</i>
+        </Card.Text>
+      </Card.Footer>
     </Card>
   );
 };
