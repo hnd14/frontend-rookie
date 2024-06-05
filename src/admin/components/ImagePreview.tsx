8c5604b05e02529a315d4543dfa64e2fce0aae0d
hnd14 <hnd14@case.edu>
2024-06-05 10:33:11 +0700

    Add view full image on click

--- src/admin/components/ImagePreview.tsx
+++ src/admin/components/ImagePreview.tsx
@@ -1,24 +1,54 @@
-import React from "react";
-import { Card, CloseButton } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, CloseButton, Image, Modal } from "react-bootstrap";
 
 const ImagePreview = ({ src, handleDelete }) => {
+  const [opacity, setOpacity] = useState("100");
+  const [showFull, setShowFull] = useState(false);
   return (
-    <Card style={{ width: "5rem", height: "5rem" }} className="text-center m-2">
-      <Card.Img variant="bottom" src={src} style={{ height: "5re  m" }} />
-      <Card.ImgOverlay>
-        <Card.Body>
-          <CloseButton
-            style={{
-              position: "absolute",
-              height: "0.25rem",
-              top: "0.25rem",
-              right: "0.25rem",
-            }}
-            onClick={handleDelete}
-          ></CloseButton>
-        </Card.Body>
-      </Card.ImgOverlay>
-    </Card>
+    <>
+      <Card
+        style={{ width: "5rem", height: "5rem" }}
+        className="text-center m-2"
+        onMouseEnter={() => {
+          setOpacity("50");
+        }}
+        onMouseLeave={() => {
+          setOpacity("100");
+        }}
+        onClick={() => {
+          setShowFull(true);
+        }}
+      >
+        <Card.Img
+          variant="bottom"
+          src={src}
+          style={{ height: "5rem" }}
+          className={`opacity-${opacity}`}
+        />
+        <Card.ImgOverlay>
+          <Card.Body>
+            <CloseButton
+              style={{
+                position: "absolute",
+                height: "0.25rem",
+                top: "0.25rem",
+                right: "0.25rem",
+              }}
+              onClick={handleDelete}
+            ></CloseButton>
+          </Card.Body>
+        </Card.ImgOverlay>
+      </Card>
+      <Modal
+        show={showFull}
+        onHide={() => {
+          setShowFull(false);
+        }}
+      >
+        <Modal.Header closeButton></Modal.Header>
+        <Image src={src}></Image>
+      </Modal>
+    </>
   );
 };
 
